067c17631cea9523203d25ad1a3212cbbb28c9a2
kadir7563483 <160729048+KadirYazadzhi@users.noreply.github.com>
2024-09-14 01:57:34 +0300

    new

---
+++ Basic Syntax, Conditional Statements and Loops/05. Login.cs
@@ -0,0 +1,37 @@
+using System;
+
+class PrintAndSum {
+    static string reverse(string name) {
+        string reverseName = "";
+        
+        for (int i = name.Length - 1; i >= 0; i--) {
+            reverseName += name[i];
+        }
+
+        return reverseName;
+    }
+
+    static void isValid(string name) {
+        int i = 1;
+        while (true) {
+             string password = Console.ReadLine();
+            if (password == reverse(name)) {
+                Console.WriteLine($"User {name} logged in.");
+                break;
+            }
+            
+            if (i > 3) {
+                Console.WriteLine($"User {name} blocked!");
+                break;
+            }
+
+            Console.WriteLine("Incorrect password. Try again.");
+            i++;
+        }
+    }
+
+    static void Main() {
+        string name = Console.ReadLine();
+        isValid(name);
+    }
+}
