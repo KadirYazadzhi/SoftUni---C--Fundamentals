d05976e39dcecb2d4256d630a71d199ae7cfbfb3
kadir7563483 <160729048+KadirYazadzhi@users.noreply.github.com>
2024-09-16 01:46:32 +0300

    new

---
+++ Basic Syntax, Conditional Statements and Loops/10. Multiplication Table.cs
@@ -0,0 +1,13 @@
+using System;
+
+class MultiplicationTable {
+    static void Main() {
+        int num = int.Parse(Console.ReadLine());
+
+        int sum = 0;
+        for (int i = 1; i <= 10; i++) {
+            sum = num * i;
+            Console.WriteLine($"{num} X {i} = {sum}");
+        }
+    }
+}
