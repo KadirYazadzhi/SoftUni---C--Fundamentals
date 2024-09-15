d7190edfa4c53b27f4916ae7c7334aa8c6042719
kadir7563483 <160729048+KadirYazadzhi@users.noreply.github.com>
2024-09-16 01:19:38 +0300

    new

---
+++ Basic Syntax, Conditional Statements and Loops/06. Foreign Languages.cs
@@ -0,0 +1,17 @@
+using System;
+
+class Language {
+    static void Main() {
+        string country = Console.ReadLine();
+
+        if (country == "England" || country == "USA") {
+            Console.WriteLine("English");
+        }
+        else if (country == "Spain" || country == "Argentina" || country == "Mexico") {
+            Console.WriteLine("Spanish");
+        }
+        else {
+            Console.WriteLine("unknown");
+        }
+    }
+}
