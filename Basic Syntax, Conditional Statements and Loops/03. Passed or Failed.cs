5c6db96d5f23798f2a07a88f9e6bd873a5ba467a
kadir7563483 <160729048+KadirYazadzhi@users.noreply.github.com>
2024-09-16 00:42:11 +0300

    new

---
+++ Basic Syntax, Conditional Statements and Loops/03. Passed or Failed.cs
@@ -0,0 +1,13 @@
+using System;
+
+class Passed {
+    static void Main() {
+        double num = double.Parse(Console.ReadLine());
+        if (num >= 3.00) {
+            Console.WriteLine("Passed!");
+        }
+        else {
+            Console.WriteLine("Failed!");
+        }
+    }
+}
