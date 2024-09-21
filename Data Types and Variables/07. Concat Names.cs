8e85080114dbf40a391dbe4db3f3268536f8cf4f
kadir7563483 <160729048+KadirYazadzhi@users.noreply.github.com>
2024-09-21 16:08:53 +0300

    new

---
+++ Data Types and Variables/07. Concat Names.cs
@@ -0,0 +1,11 @@
+using System;
+
+class ConcatNames {
+    static void Main() {
+        string firstName = Cosnole.ReadLine();
+        string lastName = Cosnole.ReadLine();
+        string separator = Console.ReadLine();
+
+        Console.WriteLine($"{firstName}{separot}{lastName}");
+    }
+}
