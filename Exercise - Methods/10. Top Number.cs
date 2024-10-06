e16d8798660ea2295f19edcd21bf1a369cc78204
kadir7563483 <160729048+KadirYazadzhi@users.noreply.github.com>
2024-10-07 00:56:55 +0300

    new

--- Exercise - Methods/10. Top Number.cs
+++ Exercise - Methods/10. Top Number.cs
@@ -2,10 +2,32 @@ using System;
 
 class TopNumbers {
     static void Main() {
+        int number = int.Parse(Console.ReadLine());
 
+        for (int i = 0; i <= number; i++) {
+            if (OddDigit(i.ToString()) && isDivisibleByEight(i.ToString())) {
+                Console.WriteLine(i);
+            }
+        }
     }
 
-    static bool OddDigit(int num) {
-        
+    static bool OddDigit(string num) {
+        foreach (char c in num) {
+            if ((c - '0') % 2 != 0) {
+                return true;
+            }
+        }
+
+        return false;
+    }
+
+    static bool isDivisibleByEight(string num) {
+        int sum = 0;
+
+        foreach (char c in num) {
+            sum += c - '0';
+        }
+
+        return sum % 8 == 0;
     }
 }
