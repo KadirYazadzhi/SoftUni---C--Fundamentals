1d2460010fa766afb403765683b8ab044a8373f5
kadir7563483 <160729048+KadirYazadzhi@users.noreply.github.com>
2024-10-11 23:34:01 +0300

    new

---
+++ Exercise - Lists/06. Cards Game.cs
@@ -0,0 +1,12 @@
+using System;
+using System.Linq;
+using System.Collections.Generic;
+
+class CardGames {
+    static void Main() {
+        List<int> numbers = Console.ReadLine().Split().Select(int.Parse).ToList();
+        int[] commandNumbers = Console.ReadLine().Split().Select(int.Parse).ToArray();
+        
+    }
+}
+
