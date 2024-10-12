6db2ad6d12336835806dba023fe24f1efab730ed
kadir7563483 <160729048+KadirYazadzhi@users.noreply.github.com>
2024-10-12 22:43:09 +0300

    new

---
+++ Exercise - Lists/09. Pokemon Don't Go.cs
@@ -0,0 +1,44 @@
+using System;
+using System.Linq;
+using System.Collections.Generic;
+
+internal class Program {
+    static void Main() {
+        List<int> list = Console.ReadLine().Split().Select(int.Parse).ToList();
+        int sum = 0;
+
+        while (list.Count > 0) {
+            int number = int.Parse(Console.ReadLine());
+
+            if (number < 0) {
+                sum += list[0];
+                ModifyList(list, list[0]);
+                list[0] = list[list.Count - 1];
+            } 
+            else if (number >= list.Count) {
+                sum += list[list.Count - 1];
+                ModifyList(list, list[list.Count - 1]);
+                list[list.Count - 1] = list[0];
+            }
+            else {
+                int currentValue = list[number]; 
+                sum += currentValue;
+                ModifyList(list, currentValue);
+                list.RemoveAt(number);
+            }
+        }
+
+        Console.WriteLine(sum);
+    }
+
+    static void ModifyList(List<int> list, int value) {
+        for (int i = 0; i < list.Count; i++) {
+            if (list[i] <= value) {
+                list[i] += value;
+            } 
+            else {
+                list[i] -= value;
+            }
+        }
+    }
+}
