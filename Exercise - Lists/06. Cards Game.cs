62ca68bf0ec2802c8fe8b3ecb1a9b120871e2cb0
kadir7563483 <160729048+KadirYazadzhi@users.noreply.github.com>
2024-10-12 22:45:29 +0300

    new

--- Exercise - Lists/06. Cards Game.cs
+++ Exercise - Lists/06. Cards Game.cs
@@ -4,9 +4,43 @@ using System.Collections.Generic;
 
 class CardGames {
     static void Main() {
-        List<int> numbers = Console.ReadLine().Split().Select(int.Parse).ToList();
-        int[] commandNumbers = Console.ReadLine().Split().Select(int.Parse).ToArray();
+        List<int> firstPlayerCards = Console.ReadLine().Split().Select(int.Parse).ToList();
+        List<int> secondPlayerCards = Console.ReadLine().Split().Select(int.Parse).ToList();
+
+        while (firstPlayerCards.Count > 0 && secondPlayerCards.Count > 0) {
+            if (firstPlayerCards[0] > secondPlayerCards[0]) {
+                firstPlayerCards.Add(firstPlayerCards[0]);
+                firstPlayerCards.RemoveAt(0);
+
+                firstPlayerCards.Add(secondPlayerCards[0]);
+                secondPlayerCards.RemoveAt(0);
+            }
+            else if (firstPlayerCards[0] < secondPlayerCards[0]) {
+                secondPlayerCards.Add(secondPlayerCards[0]);
+                secondPlayerCards.RemoveAt(0);
+
+                secondPlayerCards.Add(firstPlayerCards[0]);
+                firstPlayerCards.RemoveAt(0);
+            }
+            else {
+                firstPlayerCards.RemoveAt(0);
+                secondPlayerCards.RemoveAt(0);
+            }
+        }
         
+        if (firstPlayerCards.Count > 0) {
+            Console.WriteLine($"First player wins! Sum: {sumCards(firstPlayerCards)}");
+        }
+        else if (secondPlayerCards.Count > 0) {
+            Console.WriteLine($"Second player wins! Sum: {sumCards(secondPlayerCards)}");
+        }
+    }
+
+    static int sumCards (List<int> cards) {
+        int sum = 0;
+        foreach (int card in cards) {
+            sum += card;
+        }
+        return sum;
     }
 }
-
