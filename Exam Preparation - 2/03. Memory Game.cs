2e576b93e44939397fb61090eca31eb7183fc7b5
kadir7563483 <160729048+KadirYazadzhi@users.noreply.github.com>
2024-10-17 09:36:04 +0300

    new

--- Exam Preparation - 2/03. Memory Game.cs
+++ Exam Preparation - 2/03. Memory Game.cs
@@ -11,32 +11,40 @@ class MemoryGame {
         while ((command = Console.ReadLine()) != "end") {
             string[] commands = command.Split().ToArray();
 
-            if (characters.Count <= 0) {
+            moves++;
+            int firstIndex = int.Parse(commands[0]);
+            int secondIndex = int.Parse(commands[1]);
+
+            if (characters.Count == 0) {
                 Console.WriteLine($"You have won in {moves} turns!");
                 return;
             }
-            moves++;
-            if (isValidIndexes(characters, int.Parse(commands[0]), int.Parse(commands[1]))) {
-                if (characters[int.Parse(commands[0])] == characters[int.Parse(commands[1])]) {
-                    Console.WriteLine($"Congrats! You have found matching elements - {characters[int.Parse(commands[0])]}!");
 
-                    if (int.Parse(commands[0]) > int.Parse(commands[1])) {
-                        characters.RemoveAt(int.Parse(commands[0]));
-                        characters.RemoveAt(int.Parse(commands[1]));
+            if (isValidIndexes(characters, firstIndex, secondIndex)) {
+                if (characters[firstIndex] == characters[secondIndex]) {
+                    Console.WriteLine($"Congrats! You have found matching elements - {characters[firstIndex]}!");
+
+                    if (firstIndex < secondIndex) {
+                        characters.RemoveAt(secondIndex);
+                        characters.RemoveAt(firstIndex);
+                    } else {
+                        characters.RemoveAt(firstIndex);
+                        characters.RemoveAt(secondIndex);
                     }
-                    else {
-                        characters.RemoveAt(int.Parse(commands[1]));
-                        characters.RemoveAt(int.Parse(commands[0]) - 1);
+
+                    if (characters.Count == 0) {
+                        Console.WriteLine($"You have won in {moves} turns!");
+                        return;
                     }
-                }
+                } 
                 else {
                     Console.WriteLine("Try again!");
                 }
-            }
+            } 
             else {
                 Console.WriteLine("Invalid input! Adding additional elements to the board");
-                characters.Insert(characters.Count / 2, "-" + moves + "a");
-                characters.Insert(characters.Count / 2, "-" + moves + "a");
+                string newSymbol = "-" + moves + "a";
+                characters.InsertRange(characters.Count / 2, new List<string> { newSymbol, newSymbol });
             }
         }
 
@@ -44,10 +52,9 @@ class MemoryGame {
         Console.WriteLine(string.Join(" ", characters));
     }
 
-    static bool isValidIndexes(List<string> characters, int commandOne, int commandTwo) {
-        if (commandOne != commandTwo && commandOne >= 0 && commandTwo >= 0 && commandOne < characters.Count && commandTwo < characters.Count) {
-            return true;
-        }
-        return false;
+    static bool isValidIndexes(List<string> characters, int firstIndex, int secondIndex) {
+        return firstIndex != secondIndex &&
+               firstIndex >= 0 && firstIndex < characters.Count &&
+               secondIndex >= 0 && secondIndex < characters.Count;
     }
 }
